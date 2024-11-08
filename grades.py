6c3e6a6544c3d055461007144f932d1421d383ab
Z3NNZ <xzenatratior@gmail.com>
2024-11-08 14:19:16 +0000

    Add files via upload

---
+++ grades.py
@@ -0,0 +1,7 @@
+mark = (("input grade 100 - 0")
+        if < 101 but is > 69 = A
+        if < 68 but is > 59 = B
+        if < 58 but is > 49 = C
+        if < 48 but is > 39 = D
+        if < 38 but is > 29 = E
+        if < 28 but is > 19 = F
