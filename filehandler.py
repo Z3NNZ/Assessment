a62d37030b8a2fc2c7222ccdc6bb131e34962db9
Z3NNZ <xzenatratior@gmail.com>
2024-12-06 11:11:37 +0000

    Add files via upload

---
+++ filehandler.py
@@ -0,0 +1,7 @@
+Zenn Z, Introduction to Programming, 67, 33 
+Sara R, Introduction to Web Scripting, 44, 56
+Kasim R, Introduction to Web Development, 49, 51
+
+Zenn Z, 66, Merit
+Sara R, 90, Distinction 
+Kasim R, 88, Distinction 
